875be9b463d38f5ff3976e2e109e96e83da345db
Joshua Hoblitt <jhoblitt@cpan.org>
2014-06-02 11:48:57 -0700

    mv Hyperctl::Sysfs test setup into rspec shared_contexts

--- spec/hyperctl_sysfs_spec.rb
+++ spec/hyperctl_sysfs_spec.rb
@@ -4,88 +4,42 @@ require 'spec_helper'
 require 'pp'
 require 'fakefs'
 
-def sysfs(cpu_name, attr)
-  File.join('/sys/devices/system/cpu/', cpu_name, attr)
-end
-
-def mksysfs(cpu_name, attr, value)
-  path = sysfs(cpu_name, attr)
-  dir  = File.dirname path
-  FileUtils.mkdir_p dir
-  File.open(path, 'w+') do |f|
-    f.write(value)
-  end
-end
-
-RSpec::Matchers.define :contain do |content|
-  match do |path|
-    File.read(path) =~ content
-  end
-end
-
 describe Hyperctl::Sysfs do
-  0.upto(15).each do |core_id|
-    next if core_id == 0
-    mksysfs("cpu#{core_id}", 'online', "1\n")
-  end
-  0.upto(7).each do |core_id|
-    mksysfs("cpu#{core_id}", 'topology/thread_siblings_list', "#{core_id},#{core_id + 8}\n")
-  end
-  8.upto(15).each do |core_id|
-    mksysfs("cpu#{core_id}", 'topology/thread_siblings_list', "#{core_id},#{core_id - 8}\n")
-  end
+  context 'on a 8 physical core w/ HT enabled system' do
+    include_context "sysfs_16core"
+    include_context "cpuinfo_16core"
+    let(:hctl) { Hyperctl::Sysfs.new }
 
-  it do
-    expect(File.exists?(sysfs('cpu1', 'online'))).to be true
-    expect(File.exists?(sysfs('cpu1', 'topology/thread_siblings_list'))).to be true
-  end
-
-  it do
-    expect(sysfs('cpu1', 'topology/thread_siblings_list')).to contain(/1,9/)
-  end
-
-  it do
-    hctl = Hyperctl::Sysfs.new
-
-    info = {}
-    0.upto(7).each do |core_id|
-      name = "cpu#{core_id}"
-      info[name.to_sym] = {
-        :core_id              => core_id,
-        :online               => true,
-        :thread_siblings_list => [ core_id + 8 ],
-        :name                 => name,
-      }
-    end
-    8.upto(15).each do |core_id|
-      name = "cpu#{core_id}"
-      info[name.to_sym] = {
-        :core_id              => core_id,
-        :online               => true,
-        :thread_siblings_list => [ core_id - 8 ],
-        :name                 => name,
-      }
+    context 'fakefs sanity checks' do
+      it { expect(File.exists?(sysfs('cpu1', 'online'))).to be true }
+      it { expect(File.exists?(sysfs('cpu1', 'topology/thread_siblings_list'))).to be true }
+      it { expect(sysfs('cpu1', 'topology/thread_siblings_list')).to contain(/1,9/) }
     end
 
-    expect(hctl.cpu_info).to eq(info)
-  end
+    describe '#new' do
+      it 'finds all the cores' do
+        expect(hctl.cpu_info).to eq(info)
+      end
+    end
 
+    describe '#disable_core' do
+      it 'disbles core_id 15' do
+        hctl.disable_core(15)
+        hctl.refresh
 
-  it do
-    hctl = Hyperctl::Sysfs.new
-    hctl.disable_core(15)
-    hctl.refresh
+        expect(sysfs('cpu15', 'online')).to contain(/^0$/)
+      end
+    end
 
-    expect(sysfs('cpu15', 'online')).to contain(/^0$/)
-  end
+    describe '#enable_core' do
+      # note that this context is highly coupled with the #disable_core context
+      it 'enables core_id 15' do
+        hctl.enable_core(15)
+        hctl.refresh
 
-  it do
-    hctl = Hyperctl::Sysfs.new
-    hctl.enable_core(15)
-    hctl.refresh
-
-    expect(sysfs('cpu15', 'online')).to contain(/^1$/)
-  end
-
+        expect(sysfs('cpu15', 'online')).to contain(/^1$/)
+      end
+    end
+  end # on a 8 physical core w/ HT enabled system
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,57 @@
 require 'hyperctl'
+
+def sysfs(cpu_name, attr)
+  File.join('/sys/devices/system/cpu/', cpu_name, attr)
+end
+
+def mksysfs(cpu_name, attr, value)
+  path = sysfs(cpu_name, attr)
+  dir  = File.dirname path
+  FileUtils.mkdir_p dir
+  File.open(path, 'w+') do |f|
+    f.write(value)
+  end
+end
+
+RSpec::Matchers.define :contain do |content|
+  match do |path|
+    File.read(path) =~ content
+  end
+end
+
+RSpec.shared_context 'sysfs_16core' do
+  0.upto(15).each do |core_id|
+    next if core_id == 0
+    mksysfs("cpu#{core_id}", 'online', "1\n")
+  end
+  0.upto(7).each do |core_id|
+    mksysfs("cpu#{core_id}", 'topology/thread_siblings_list', "#{core_id},#{core_id + 8}\n")
+  end
+  8.upto(15).each do |core_id|
+    mksysfs("cpu#{core_id}", 'topology/thread_siblings_list', "#{core_id},#{core_id - 8}\n")
+  end
+end
+
+RSpec.shared_context 'cpuinfo_16core' do
+  info = {}
+  0.upto(7).each do |core_id|
+    name = "cpu#{core_id}"
+    info[name.to_sym] = {
+      :core_id              => core_id,
+      :online               => true,
+      :thread_siblings_list => [ core_id + 8 ],
+      :name                 => name,
+    }
+  end
+  8.upto(15).each do |core_id|
+    name = "cpu#{core_id}"
+    info[name.to_sym] = {
+      :core_id              => core_id,
+      :online               => true,
+      :thread_siblings_list => [ core_id - 8 ],
+      :name                 => name,
+    }
+  end
+
+  let(:info) { info }
+end
